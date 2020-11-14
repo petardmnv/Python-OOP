a68e30aa92753a3e827ab6d9a9c5da5a591a599b
Peter <36786686+petardmnv@users.noreply.github.com>
2020-11-14 12:23:51 +0200

    Add working exercise.

--- Exercise Encapsulation/main.py
+++ Exercise Encapsulation/main.py
@@ -1,16 +1,76 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from project.keeper import Keeper
+from project.caretaker import Caretaker
+from project.cheetah import Cheetah
+from project.lion import Lion
+from project.tiger import Tiger
+from project.vet import Vet
+from project.zoo import Zoo
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+zoo = Zoo("Zootopia", 3000, 5, 8)
+
+
+
+# Animals creation
+
+animals = [Cheetah("Cheeto", "Male", 2), Cheetah("Cheetia", "Female", 1), Lion("Simba", "Male", 4), Tiger("Zuba", "Male", 3), Tiger("Tigeria", "Female", 1), Lion("Nala", "Female", 4)]
+
+
+
+# Animal prices
+
+prices = [200, 190, 204, 156, 211, 140]
+
+
+
+# Workers creation
+
+workers = [Keeper("John", 26, 100), Keeper("Adam", 29, 80), Keeper("Anna", 31, 95), Caretaker("Bill", 21, 68), Caretaker("Marie", 32, 105), Caretaker("Stacy", 35, 140), Vet("Peter", 40, 300), Vet("Kasey", 37, 280), Vet("Sam", 29, 220)]
+
+
+
+# Adding all animals
+
+for i in range(len(animals)):
+
+    animal = animals[i]
+
+    price = prices[i]
+
+    print(zoo.add_animal(animal, price))
+
+
+
+# Adding all workers
+
+for worker in workers:
+
+    print(zoo.hire_worker(worker))
+
+
+
+# Tending animals
+
+print(zoo.tend_animals())
+
+
+
+# Paying keepers
+
+print(zoo.pay_workers())
+
+
+
+# Fireing worker
+
+print(zoo.fire_worker("Adam"))
+
+
+
+# Printing statuses
+
+print(zoo.animals_status())
+
+print(zoo.workers_status())
+
--- Exercise Encapsulation/project/zoo.py
+++ Exercise Encapsulation/project/zoo.py
@@ -4,32 +4,36 @@ from project.cheetah import Cheetah
 from project.lion import Lion
 from project.tiger import Tiger
 from project.vet import Vet
+from typing import Union
+Animals = Union[Cheetah, Lion, Tiger]
+Workers = Union[Keeper, Vet, Caretaker]
 
 
 class Zoo:
 
-    def __init__(self, name: str, bugdet: int, animal_capacity: int, workers_capacity: int):
+    def __init__(self, name: str, budget: int, animal_capacity: int, workers_capacity: int):
         self.name = name
-        self.__budget = bugdet
+        self.__budget = budget
         self.__animal_capacity = animal_capacity
         self.__workers_capacity = workers_capacity
         self.animals = []
         self.workers = []
 
-    def add_animal(self, animal , price: int):
+    def add_animal(self, animal: Animals, price: int):
         if self.__budget < price:
             return "Not enough budget"
-        elif self.__animal_capacity >= len(self.animals):
+        elif self.__animal_capacity <= len(self.animals):
             return "Not enough space for animal"
         else:
             self.animals.append(animal)
             self.__budget -= price
             return f"{animal.name} the {animal.get_type()} added to the zoo"
 
-    def hire_worker(self, worker):
-        if self.__workers_capacity >= len(self.workers):
+    def hire_worker(self, worker: Workers):
+        if self.__workers_capacity <= len(self.workers):
             return "Not enough space for worker"
         else:
+            self.workers.append(worker)
             return f"{worker.name} the {worker.get_type()} hired successfully"
 
     def fire_worker(self, worker_name: str):
@@ -40,6 +44,66 @@ class Zoo:
             return f"{worker_name} fired successfully"
         else:
             return f"There is no {worker_name} in the zoo"
+
+    def pay_workers(self):
+        salaries = sum([w.salary for w in self.workers])
+        if salaries <= self.__budget:
+            self.__budget -= salaries
+            return f"You payed your workers. They are happy. Budget left: {self.__budget}"
+        else:
+            return "You have no budget to pay your workers. They are unhappy"
+
+    def tend_animals(self):
+        tend_cost = sum([a.get_needs() for a in self.animals])
+        if tend_cost <= self.__budget:
+            self.__budget -= tend_cost
+            return f"You tended all the animals. They are happy. Budget left: {self.__budget}"
+        else:
+            return "You have no budget to tend the animals. They are unhappy."
+
+    def profit(self, amount: int):
+        self.__budget += amount
+
+    def animals_status(self):
+        result = list()
+        result.append(f"You have {len(self.animals)} animals")
+        lions = [l for l in self.animals if l.get_type() == "Lion"]
+        result.append(f"----- {len(lions)} Lions:")
+        for l in lions:
+            result.append(repr(l))
+
+        tigers = [t for t in self.animals if t.get_type() == "Tiger"]
+        result.append(f"----- {len(tigers)} Tigers:")
+        for t in tigers:
+            result.append(repr(t))
+
+        cheetah = [c for c in self.animals if c.get_type() == "Cheetah"]
+        result.append(f"----- {len(cheetah)} Cheetahs:")
+        for c in cheetah:
+            result.append(repr(c))
+
+        return '\n'.join(result)
+
+    def workers_status(self):
+        result = list()
+        result.append(f"You have {len(self.workers)} workers")
+
+        keepers = [k for k in self.workers if k.get_type() == "Keeper"]
+        result.append(f"----- {len(keepers)} Keepers:")
+        for k in keepers:
+            result.append(repr(k))
+
+        caretakers = [c for c in self.workers if c.get_type() == "Caretaker"]
+        result.append(f"----- {len(caretakers)} Caretakers:")
+        for c in caretakers:
+            result.append(repr(c))
+
+        vets = [v for v in self.workers if v.get_type() == "Vet"]
+        result.append(f"----- {len(vets)} Vets:")
+        for v in vets:
+            result.append(repr(v))
+
+        return '\n'.join(result)
 
     def get_worker_by_name(self, worker_name: str):
         for w in self.workers:
